d78577cfc9254896f6ae7366e629c7d994645ff8
blogum <39789527+blogum@users.noreply.github.com>
2018-05-31 19:09:21 -0700

    updates the song name

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ btn.addEventListener('click', function() {
 	var input = document.querySelector('#input');
 	if (input.files.length == 0) {
 		alert("Upload at least one JSON file");
-		//return;
+		return;
 	}
 	var no_ducks = document.querySelector('#delete-ducks').checked;
 	var no_walls = document.querySelector('#delete-walls').checked;
@@ -24,43 +24,58 @@ btn.addEventListener('click', function() {
 	function onReaderLoad(event, file) {
         var notes = '--' + file.name + '--\n';
 
-		try {
-	        var obj = JSON.parse(event.target.result);
-	    }
-	    catch(error) {
-        	notes += 'Error - Not a valid JSON file\nFAILED\n\n';
-        	addNote(notes);
-        	return;
-	    }
-
-        if (!obj._notes || !obj._obstacles) {
-        	notes += 'Error - Not a valid song\nFAILED\n\n';
-        	addNote(notes);
-        	return;
+	try {
+        var obj = JSON.parse(event.target.result);
+        }
+        catch(error) {
+                notes += 'Error - Not a valid JSON file\nFAILED\n\n';
+                addNote(notes);
+                return;
         }
 
-        if (no_ducks) {
-        	var obstacles = [];
-        	for (var i = 0; i < obj._obstacles.length; i++) {
-        		if (obj._obstacles[i]._type != 1) {
-        			obstacles.push(obj._obstacles[i]);
-        		}
-        	}
-        	obj._obstacles = obstacles;
+        if ((!obj._notes || !obj._obstacles) && !obj.songName) {
+        	notes += 'Error - Not a valid song or info file\nFAILED\n\n';
+        	addNote(notes);
+        	return;
         }
-        if (no_walls) {
-        	obj._obstacles = [];
+
+        if (obj._notes) {
+                // song file
+                if (no_ducks) {
+                        var obstacles = [];
+                        for (var i = 0; i < obj._obstacles.length; i++) {
+                                if (obj._obstacles[i]._type != 1) {
+                                        obstacles.push(obj._obstacles[i]);
+                                }
+                        }
+                        obj._obstacles = obstacles;
+                }
+                if (no_walls) {
+                        obj._obstacles = [];
+                }
+                if (no_bombs) {
+                        var note_blocks = [];
+                        for (var i = 0; i < obj._notes.length; i++) {
+                                if (obj._notes[i]._type != 3) {
+                                        note_blocks.push(obj._notes[i]);
+                                }
+                        }
+                        obj._notes = note_blocks;
+                }
         }
-        if (no_bombs) {
-        	var note_blocks = [];
-        	for (var i = 0; i < obj._notes.length; i++) {
-        		if (obj._notes[i]._type != 3) {
-        			note_blocks.push(obj._notes[i]);
-        		}
-        	}
-        	obj._notes = note_blocks;
+        else {
+                // info file
+                var description = " (edit";
+                if (no_ducks)
+                        description += ", no ducks";
+                if (no_walls)
+                        description += ", no walls";
+                if (no_bombs)
+                        description += ", no bombs";
+                description += ')';
+                obj.songName = obj.songName + description;
         }
-		
+	
 	var a = document.createElement("a");
 	var json = new Blob([JSON.stringify(obj)], {type: 'text/plain'});
 	a.href = URL.createObjectURL(json);
